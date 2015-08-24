34ee65f7b9b1b89858b6a050f6ae310b643682a3
mongoose11235813 <mongoose11235813@gmail.com>
2015-08-24 16:47:25 +0300

    5th_level fixed bugs. now it runs

--- levels/05_theCorridor.jsx
+++ levels/05_theCorridor.jsx
@@ -11,7 +11,7 @@
 
 function startLevel(map) {
 #START_OF_START_LEVEL#
-    map.displayChapter('Chapter 5\nChapters are supposed to be\nmore than one level long');
+    map.displayChapter('Chapter 5\nChapters are supposed to be more than one level long');
 
     map.defineObject('trap_left', {
         'type': 'dynamic',
@@ -19,7 +19,7 @@ function startLevel(map) {
         'color': '#900',
         'impassable': 'true',
         'behavior': function (me) {
-            trap_behaviour(me, 1, 4);
+            trap_behaviour(me, 1, 6);
         }
     }
     );
@@ -29,7 +29,7 @@ function startLevel(map) {
         'color': '#900',
         'impassable': 'true',
         'behavior': function (me) {
-            trap_behaviour(me, -3, 0);
+            trap_behaviour(me, -5, 0);
         }
     }
     );
@@ -47,30 +47,30 @@ function startLevel(map) {
         if (player_pos.y - me.getY() <= 1 && !me.trapTriggered) {
             me.trapTriggered = true;
             for (var x = left; x < right; ++x) {
-                map.placeObject(x, me.getY(), 'laser')
+                map.placeObject(me.getX() + x, me.getY(), 'laser')
             }
         }
     }
 
     var level_map = [
-        '#######'
-        '#  x  #'
-        '#     #'
-        '>     #'
-        '#     #'
-        '#     <'
-        '#     #'
-        '>     #'
-        '#     #'
-        '#     <'
-        '#     #'
-        '#  g e#'
+        '#######',
+        '#  x  #',
+        '#     #',
+        '>     #',
+        '#     #',
+        '#     <',
+        '#     #',
+        '>     #',
+        '#     #',
+        '#     <',
+        '#     #',
+        '#  @ e#',
         '#######'
     ]
     var width = map.getWidth();
     var height = map.getHeight();
-    var map_left = (width - level_map[0].length) % 2;
-    var map_top = (height - level_map.length) % 2;
+    var map_left = Math.floor((width - level_map[0].length) / 2);
+    var map_top = Math.floor((height - level_map.length) / 2);
     map.createFromGrid(level_map, {
       'x': 'exit',
       '#': 'block',
